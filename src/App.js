f53d46e98d5ea8c7a1c5cdbbfe65b091c4bd3e3a
nuka99 <boragu1234@gmail.com>
2024-10-02 20:58:16 +0900

    2-3진행

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+/*eslint-disable*/
+
 import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useState } from 'react';
 
 
 function App() {
+
+  // data.js에 들어있는 data 가져와서 useState 사용하기
+  let [shoes] = useState(data)
+
   return (
     <div className="App">
     <Navbar bg="dark" data-bs-theme="dark">
@@ -20,9 +28,41 @@ function App() {
         </Container>
       </Navbar>
 
+      <div className='main-bg'></div>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <product />
+          </div>
+          <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content}</p>
+            <p>{shoes[1].price} 원</p>
+          </div>
+          <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].content}</p>
+            <p>{shoes[2].price} 원</p>
+          </div>
+        </div>
+      </div>
+
       
     </div>
   );
 }
 
+function product(props){
+  return(
+    <div className='col-md-4'>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content}</p>
+            <p>{shoes[0].price} 원</p>
+          </div>
+  )
+}
 export default App;
